73cdd156503e3a9f2639047929ed2514a0afcb0d
Navyasri100 <Kondaparthi.NavyaSri@thomsonreuters.com>
2026-06-23 23:13:03 +0530

    Update reservation service imports

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
 import { Reservation } from '../models/models';
 import { environment } from '../../environments/environment';
 
